5c5e5d317d8ad632514c41e071a5bc6bae811a8c
christc3194 <101961721+christc3194@users.noreply.github.com>
2022-03-19 21:47:53 -0700

    Add files via upload

---
+++ MovieTheaterSeats.java.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class MyPillowIsAwesome {
+	public static void main(String[] args) {
+		// find all seats in the movie theater!
+		int numRows;
+		int currRow;
+		
+		int numColumns;
+		int currColumn;
+		char columnLetter;
+		
+		Scanner scnr = new Scanner(System.in);
+		
+		System.out.print("Please enter number of rows: ");
+		numRows = scnr.nextInt();
+		System.out.print("\n");
+		System.out.print("Please enter number of columns: ");
+		numColumns = scnr.nextInt();
+		System.out.print("\n");
+		
+		print("All seats in the movie theatre: ");
+		
+		for (currRow = 1; currRow <= numRows; ++currRow) {
+			columnLetter = 'A';
+			for (currColumn = 1; currColumn <= numColumns; ++currColumn) {
+				System.out.print(currRow + "" + columnLetter + "" + " ");
+				++currColumn;
+			}
+			++currRow;
+		}
+		print("\n");
